51dcd3b726bd65716b27d28f21be66870fa7e178
Thiago Cordeiro <thiagocordeirooo@gmail.com>
2017-05-26 11:14:10 -0300

    Subindo api

--- api/server.js
+++ api/server.js
@@ -1,1 +1,24 @@
-{}
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/todoListModel'),
+  bodyParser = require('body-parser');
+  
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://tac:123456@ds151461.mlab.com:51461/todo-list');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
+
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
