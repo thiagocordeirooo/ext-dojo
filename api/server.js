5579e499ba86b1c06df1cc1b8db6b47090f744e6
Thiago Cordeiro <thiagocordeirooo@gmail.com>
2017-05-26 11:33:43 -0300

    Merge branch 'master' into pacotao

--- api/server.js
+++ api/server.js
@@ -4,21 +4,17 @@ var express = require('express'),
   mongoose = require('mongoose'),
   Task = require('./api/models/todoListModel'),
   bodyParser = require('body-parser');
-  
+
 mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://tac:123456@ds151461.mlab.com:51461/todo-list');
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 
 var routes = require('./api/routes/todoListRoutes');
 routes(app);
 
-
 app.listen(port);
-
 
 console.log('todo list RESTful API server started on: ' + port);
